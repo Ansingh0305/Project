ea9d2bcc880f74df0b12c98a93d98ece59b38815
Ansingh0305 <59765993+Ansingh0305@users.noreply.github.com>
2020-04-28 12:41:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -106,10 +106,10 @@ async function getBackgroundImg(){
     var hour = datetime.slice(11,13);
     
     if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
+        bg = "sprites/bg2.jpg";
     }
     else{
-        bg = "sprites/bg2.jpg";
+        bg = "sprites/bg1.png";
     }
 
     backgroundImg = loadImage(bg);
